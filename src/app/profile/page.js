8ecd890c184f25039a1244c504dfe914324bf8ea
wenxuanh <wenxuanhuang5@gmail.com>
2025-11-09 03:48:42 -0500

    navbar to learn and profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,7 +1,15 @@
+import Navbar from "../../components/navbar/navbar";
+
 export default function Profile() {
   return (
-    <div>
-      <p>hello world</p>
+    <div className="flex min-h-screen">
+      {/* Left sidebar */}
+      <Navbar />
+
+      {/* Main content */}
+      <main style={{ marginLeft: "6rem", padding: "2rem" }}>
+        <p>hello world</p>
+      </main>
     </div>
   );
 }
