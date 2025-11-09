6a40b5e2628d1a6cea5657fd20bf1653c12556f6
wenxuanh <wenxuanhuang5@gmail.com>
2025-11-08 19:17:59 -0500

    responsive navbar complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
+import Navbar from "./components/navbar/navbar";
+
 
 export default function Home() {
   return (
-    <div>
-      <p>hello world</p>
+    <div className="flex min-h-screen">
+      {/* Left sidebar */}
+      <Navbar />
+
+      {/* Main content */}
+      <main style={{ marginLeft: "6rem", padding: "2rem" }}>
+        <p>hello world</p>
+      </main>
     </div>
   );
 }
